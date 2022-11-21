8e98cd4e7c796e068de60eb5285596ce86c87a70
omarigrant <ogrant92@gmail.com>
2022-11-21 18:06:24 +0000

    Added Exception Handling to Greeting (Main.java)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,18 +1,41 @@
 package org.example;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-//        Scanner scan = new Scanner(System.in);
-//        System.out.println("Enter int");
-//        int input = scan.nextInt();
-        int input = 1;
+        int input = input();
+
+
+        while (input < 1 || input >24){
+            System.out.println("Invalid input - Please try again using 1-24");
+            input = input();
+        }
+
+//        int input = 1;
 
         System.out.println(greeting(input));
 
     }
+
+    protected static int input(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter Time in Hours (e.g 11 for 11am and 23 for 11pm)");
+        int input = -1;
+
+        try {
+            input = scan.nextInt();
+
+        } catch (InputMismatchException ime){
+            System.out.println("Input Error, Please retry with an integer between 1-24.");
+            input();
+        }
+
+        return input;
+    }
+
 
     protected static String greeting(int input){
         
